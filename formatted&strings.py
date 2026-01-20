9007979b7d0dcf52b78f430a0e26809be363e7f4
Iommi <daniel.markos666@gmail.com>
2026-01-20 23:08:17 +0000

    strings lessons

---
+++ formatted&strings.py
@@ -0,0 +1,17 @@
+first = "Daniel"
+last = "Rocha"
+full = f"{first} {last}"
+print (full)
+#O f antes da string permite interpolação de variáveis e expressões usando {} dentro da string
+
+course = "   Python programming"
+print(course.upper())
+print (course.lower())
+print(course.title())
+print(course.strip())
+print(course.lstrip())
+print(course.find("pro"))
+print(course.replace("p", "j"))
+print("pro" in course)
+print("switft" not in course)
+#metódos de string
---
+++ variaveis_lesson.py
@@ -0,0 +1,27 @@
+students_count = 1000
+students_score = 5.0
+students_course = "Python Programming"
+#variáveis
+
+#Python é sensível a maiúsculas e minúsculas
+valor = 10
+Valor = 20
+
+#Não é necessário declarar o tipo
+#Python é dinamicamente tipado:
+x = 10      # inteiro
+x = "dez"   # agora é string
+
+#Variáveis são referências (binding)
+#A variável não guarda o valor, ela aponta para um objeto na memória.
+a = 10
+b = a
+a = 20
+
+#Constantes (convenção)
+#Python não tem constantes reais, mas usa-se letras maiúsculas:
+PI = 3.14
+TAXA_JUROS = 0.05
+
+#Múltiplas atribuições
+a, b, c = 1, 2, 3
